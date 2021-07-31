c079d73e07cac65edd40e39cf4af3caf0d800843
Michael Schori <michael.schori@gmx.ch>
2021-07-31 17:31:05 +0200

    Max-Length of URL-Fields is now 500

--- wedding/models.py
+++ wedding/models.py
@@ -5,7 +5,7 @@ class WeddingImage(models.Model):
     """
     Model for Wedding-Images.
     """
-    url = models.URLField(null=False)
+    url = models.URLField(null=False, max_length=500)
 
 
 class WeddingVideo(models.Model):
@@ -13,5 +13,5 @@ class WeddingVideo(models.Model):
     Model for Wedding-Videos.
     """
     name = models.CharField(max_length=50, null=False)
-    url = models.URLField(null=False)
-    image_url = models.URLField(null=False)
+    url = models.URLField(null=False, max_length=500)
+    image_url = models.URLField(null=False, max_length=500)
